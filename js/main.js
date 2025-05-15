7ebe2a1dabff151faea34a534ff665615263a741
thobui997 <thobui97@outlook.com>
2025-05-15 17:27:41 +0700

    Add new images, update HTML for navigation and buttons, and implement destination slider
    - Added new image files: shutterstock_11.jpg and vietnam-01.png.
    - Updated index.html to change the "BOOKING NOW" button class for better styling.
    - Changed the "PACKAGES" link to point to destinations.html.
    - Enhanced the review button with a new class for improved appearance.
    - Implemented a new slick slider for the destination list in main.js with responsive settings.

--- js/main.js
+++ js/main.js
@@ -44,4 +44,37 @@ $(document).ready(function () {
     dotsClass: "slick-dots slick-dots__review",
     pauseOnDotsHover: true,
   });
+
+  $(".destination-list").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    infinite: false,
+    prevArrow: `<button type="button" class="slick-prev"><img src="./images/arrow-1.png" alt=""></button>`,
+    nextArrow: `<button type="button" class="slick-next"><img src="./images/arrow-1-1.png" alt=""></button>`,
+    autoplay: false,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+    ],
+  });
 });
